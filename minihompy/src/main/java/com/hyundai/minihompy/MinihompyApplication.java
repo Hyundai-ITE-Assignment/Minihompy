7d8d18b4b8bd343f0b9d97fca7d4afa2d96a099f
PARKJUYOUNG <juzero98@naver.com>
2022-05-11 16:48:27 +0900

    Init Spring Boot Project

---
+++ minihompy/src/main/java/com/hyundai/minihompy/MinihompyApplication.java
@@ -0,0 +1,13 @@
+package com.hyundai.minihompy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinihompyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinihompyApplication.class, args);
+	}
+
+}
